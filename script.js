4fcc348fb5c86ab04e029a13dd70af14739f52cd
ibrahimaupian09 <ibrahimaupian09@gmail.com>
2024-09-20 22:03:18 +0500

    done the header section

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const hamburger = document.querySelector(".hamburger");
 
+const mobileMenu = document.querySelector(".nav-list ul")
 hamburger.addEventListener("click", () =>  {
     hamburger.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
 });
+
